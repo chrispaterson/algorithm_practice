d6726896fc720078591b928011d4f0edbb2fa345
Chris Paterson <cpaterson@conversantmedia.com>
2018-11-16 15:57:38 -0800

    Adding comments

--- mergeArrays.js
+++ mergeArrays.js
@@ -17,14 +17,28 @@ console.log(mergeArrays(myArray, alicesArray));
 
 //  solution O(n)
 function mergeArrays(ar1, ar2) {
-  var len = ar1.length + ar2.length - 1;
+
+  // our new array length will be the sum of the
+  // two sub arrays
+  var len = ar1.length + ar2.length;
+
+  // might as well set the number of elements
+  // ahead of time so we can allow JS to not 
+  // for a dynamic resize
   var retr = new Array(len);
+
+  // create our indices for tracking our progress
+  // int he sorted arrays
   var ar1Idx = 0;
   var ar2Idx = 0;
-  var i = 0;
 
-  for(i; i < len; i ++) {
+  // loop through the length of our return array
+  for(var i = 0; i < len; i ++) {
 
+    // if the value in array 2 is less than the 
+    // value in array 1 then we'll put that value
+    // into a return value first and then increment
+    // our index for that array
     if(ar1[ar1Idx] > ar2[ar2Idx]) {
 
       retr[i] = ar2[ar2Idx];
@@ -33,6 +47,20 @@ function mergeArrays(ar1, ar2) {
 
       retr[i] = ar1[ar1Idx];
       ar1Idx++;
+    }
+
+    // are we on the last loop?  If yes then
+    // one will be orphaned, we have to find
+    // which one it is.
+    if(i === len - 1) {
+
+      if(ar1[ar1Idx] !== undefined) {
+
+        retr[i] = ar1[ar1Idx];
+      } else {
+
+        retr[i] = ar2[ar2Idx];
+      }
     }
   }
   return retr;
