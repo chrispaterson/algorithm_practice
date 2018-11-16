07d52ed7c4f7f385017eecdd5459a70658ad3257
Chris Paterson <cpaterson@conversantmedia.com>
2018-11-16 15:33:57 -0800

    Adding prompts

--- mergeArrays.js
+++ mergeArrays.js
@@ -1,6 +1,21 @@
+// In order to win the prize for most cookies sold, 
+// my friend Alice and I are going to merge our 
+// Girl Scout Cookies orders and enter as one unit.
+//
+// Each order is represented by an "order id" 
+// (an integer).
+//
+// We have our lists of orders sorted numerically 
+// already, in arrays. Write a function to merge 
+// our arrays of orders into one sorted array.
+//
+// For Example:
 var myArray     = [3, 4, 6, 10, 11, 15];
 var alicesArray = [1, 5, 8, 12, 14, 19];
 
+console.log(mergeArrays(myArray, alicesArray));
+
+//  solution O(n)
 function mergeArrays(ar1, ar2) {
   var len = ar1.length + ar2.length - 1;
   var retr = new Array(len);
@@ -23,5 +38,3 @@ function mergeArrays(ar1, ar2) {
   return retr;
 
 }
-
-console.log(mergeArrays(myArray, alicesArray));
--- reverseWords.js
+++ reverseWords.js
@@ -1,5 +1,52 @@
+//You're working on a secret team solving coded transmissions.
+//
+// Your team is scrambling to decipher a recent message,
+// worried it's a plot to break into a major European National
+// Cake Vault. The message has been mostly deciphered, but all
+// the words are backward! Your colleagues have handed off the
+// last step to you.
+//
+// Write a function reverseWords() that takes a message as an
+// array of characters and reverses the order of the words in
+// place.
+//
+// For Example:
+const message = [ 'c', 'a', 'k', 'e', ' ',
+  'p', 'o', 'u', 'n', 'd', ' ',
+  's', 't', 'e', 'a', 'l' ];
+
+reverseWords(message);
+
+console.log(message.join(''));
+
+// Solution O(n) 
+
 // reverse the string in place based on a right and left
 // boundary (basically a swap function)
+function reverseWords(message) {
+
+  let lastLeft = 0;
+
+  // first reverse the entire message
+  reverseSegment(0, message.length - 1, message);
+
+  // now go through and reverse each word back to the original
+  // direction at word boundaries or at the edge case of it
+  // being the end of the string
+  for(let i = 0; i < message.length; i++) {
+
+    if(message[i] === " ") {
+
+      reverseSegment(lastLeft, i - 1, message);
+      i ++;
+      lastLeft = i;
+    } else if (i === message.length - 1) {
+
+      reverseSegment(lastLeft, i, message);
+    }
+  }
+}
+
 function reverseSegment(left, right, str) {
   let temp;
   while(right > left) {
@@ -9,27 +56,4 @@ function reverseSegment(left, right, str) {
     left += 1;
     right -= 1;
   }
-}
-function reverseWords(message) {
-
-    let lastLeft = 0;
-    
-    // first reverse the entire message
-    reverseSegment(0, message.length - 1, message);
-
-    // now go through and reverse each word back to the original 
-    // direction at word boundaries or at the edge case of it
-    // being the end of the string 
-    for(let i = 0; i < message.length; i++) {
-      
-      if(message[i] === " ") {
-        
-        reverseSegment(lastLeft, i - 1, message);
-        i ++;
-        lastLeft = i;
-      } else if (i === message.length - 1) {
-        
-        reverseSegment(lastLeft, i, message);
-      }
-    }
 }
