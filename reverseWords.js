e009cd8214b1d7d438778afb8ab82e4773d95225
Chris Paterson <cpaterson@conversantmedia.com>
2018-11-16 13:59:16 -0800

    Cleaning up the code a big

--- reverseWords.js
+++ reverseWords.js
@@ -1,31 +1,36 @@
+
+// reverse the string in place based on a right and left
+// boundary (basically a swap function)
+function reverseSegment(left, right, str) {
+  let temp;
+  while(right > left) {
+    temp = str[left];
+    str[left] = str[right]
+    str[right] = temp;
+    left += 1;
+    right -= 1;
+  }
+}
 function reverseWords(message) {
 
-  // Decode the message by reversing the words
-  
-    let reverseSegment = function(left, right, str) {
-      let temp;
-      while(right > left) {
-        temp = str[left];
-        str[left] = str[right]
-        str[right] = temp;
-        left += 1;
-        right -= 1;
-      }
-    }
-    let lastIndex = 0;
+    let lastLeft = 0;
     
+    // first reverse the entire message
     reverseSegment(0, message.length - 1, message);
 
+    // now go through and reverse each word back to the original 
+    // direction at word boundaries or at the edge case of it
+    // being the end of the string 
     for(let i = 0; i < message.length; i++) {
       
       if(message[i] === " ") {
         
-        reverseSegment(lastIndex, i - 1, message);
+        reverseSegment(lastLeft, i - 1, message);
         i ++;
-        lastIndex = i;
+        lastLeft = i;
       } else if (i === message.length - 1) {
         
-        reverseSegment(lastIndex, i, message);
+        reverseSegment(lastLeft, i, message);
       }
     }
 }
