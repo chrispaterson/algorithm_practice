de730e2dc0ad4f4411dffa177e003af38de5ae2b
Chris Paterson <cpaterson@conversantmedia.com>
2018-11-20 19:15:40 -0800

    Adding comments

--- countSort.js
+++ countSort.js
@@ -1,17 +1,33 @@
+// arr is the array to sort
+// maximum value is the maximum value in the array
 function countSort(arr, maxVal) {
 
+  // create an array with the number of times a number
+  // appears in arr
   const counts = new Array(maxVal + 1).fill(0);
 
+  // count each occurrence of a number by looping
+  // through the unsorted arr and then incrementing
+  // a number in our counts array
   arr.forEach(val => {
     counts[val] ++;
   });
 
+  // create our final returned array.  This is not
+  // an in-place sorting algorithm
   const retr = [];
 
+  // loop through backwards between all the
+  // possible values of the array so we push the 
+  // highest value towards the front of the array.
   for(let val = maxVal; val >= 0; val--) {
+
+    // number of times this value occurs
     const count = counts[val];
 
-    for(let j = 0; j < count; j++) {
+    // add this value to the final output array
+    // for every time it occurs in the original
+    for(let times = 0; times < count; times++) {
       retr.push(val);
     }
   }
