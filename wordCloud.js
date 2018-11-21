fa0a387a961236b15d01d777c2a0821fa03884d6
Chris Paterson <cpaterson@conversantmedia.com>
2018-11-20 18:45:41 -0800

    handling more cases

--- wordCloud.js
+++ wordCloud.js
@@ -1,8 +1,8 @@
-//You want to build a word cloud, an infographic where the size of a word 
+//You want to build a word cloud, an infographic where the size of a word
 //corresponds to how often it appears in the body of text.
 //
-// To do this, you'll need data. Write code that takes a long string and builds 
-// its word cloud data in a map, where the keys are words and the values are 
+// To do this, you'll need data. Write code that takes a long string and builds
+// its word cloud data in a map, where the keys are words and the values are
 // the number of times the words occurred.
 
 // O(2n)
@@ -31,30 +31,49 @@ function wordCloud(input) {
 
   let map = new Map();
   let temp = "";
+  let newWords = new Set([" ", "-"])
 
   for(let i = 0; i < input.length; i++) {
 
+    // get the char and the code
     let char = input[i];
     let charCode = char.charCodeAt(0);
 
     // it's an alpha char
     if((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
 
+      // build our temp var since it's an alpha
       temp += char;
-    } else if(char === " " || char === "\n" || i === input.length - 1) {
+    } else if(newWords.has(char) || i === input.length - 1) {
 
+      if(temp !== "") {
+        temp = temp.toLowerCase();
+        if(map.has(temp)) {
+
+          map.set(temp, map.get(temp) + 1);
+        } else {
+
+          map.set(temp, 1);
+        }
+        temp = "";
+      }
+      // check if it's an elipsis
+    } else if(char === "." && input[i + 1] === char && input[i + 2] === char) {
+
+      i += 2;
+      temp = temp.toLowerCase();
       if(map.has(temp)) {
 
         map.set(temp, map.get(temp) + 1);
       } else {
 
         map.set(temp, 1);
-        temp = "";
       }
+      temp = "";
     }
   }
 
   return map;
 }
 
-console.log(wordCloud("After beating the eggs, Dana read the next step:"));
+console.log(wordCloud("We came, we saw, we conquered...then we ate Bill's (Mille-Feuille) cake."));
