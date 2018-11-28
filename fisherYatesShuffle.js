b64d35f84defd4a9c0417a1272e6c0c3145588e2
Chris Paterson <cpaterson@conversantmedia.com>
2018-11-27 17:23:35 -0800

    Adding fisher-yates shuffle algorithm

---
+++ fisherYatesShuffle.js
@@ -0,0 +1,21 @@
+function shuffle(arr) {
+
+  for(let i = arr.length - 1; i >= 0; i--) {
+
+    let rand = Math.round(Math.random() * i);
+
+    let temp = arr[i];
+    arr[i] = arr[rand];
+    arr[rand] = temp;
+
+  }
+  return arr;
+}
+
+// make a test deck
+let deck = [];
+for(let i = 1; i <= 52; i++) {
+  deck.push(i);
+}
+
+console.log(shuffle(deck));
